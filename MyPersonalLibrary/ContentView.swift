a8299cf08603b68ff808e7bbcaf5f16e48c1a279
tsouza <tsouza@expediagroup.com>
2022-09-08 14:50:22 +0200

    Initial Commit

---
+++ MyPersonalLibrary/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyPersonalLibrary
+//
+//  Created by Tarsha de Souza on 8/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyPersonalLibrary/MyPersonalLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPersonalLibraryApp.swift
+//  MyPersonalLibrary
+//
+//  Created by Tarsha de Souza on 8/9/22.
+//
+
+import SwiftUI
+
+@main
+struct MyPersonalLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
